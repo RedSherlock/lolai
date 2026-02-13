701e610ccc51078116c95a9f0d5e4ce5e1213663
RedSherlock <ghgus1577@gmail.com>
2026-02-13 17:14:07 +0900

    수정2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+
 function App() {
   return(
+    <div className='App'>
 
-  <div className='App'>
-    <div className ='black-nav'>
-      <div className='logo'>lolAI.gg</div>
-      <h4>순위표</h4>
-      <h4>AI예측</h4>
-      <h4>등급표</h4>
-      <h4>게유게시판</h4>
-      <h4>로그인</h4>
-  </div>
-    <div className="main-image">
-      <div className="image-wrapper">
-        <img src="/images/lolAI.png" alt="ai vs user" />
+      <div className ='black-nav'>
+        <div className='logo'>lolAI.gg</div>
+        <h4>순위표</h4>
+        <h4>AI예측</h4>
+        <h4>등급표</h4>
+        <h4>게유게시판</h4>
+        <h4>로그인</h4>
       </div>
+
+      <div className="main-image">
+        <div className="image-wrapper">
+          <img src="/images/lolAI.png" alt="ai vs user" />
+        </div>
+
+        {/* 🔥 여기 추가 */}
+        <div className="rank-box">
+          <div className="rank-tier">GOLD</div>
+          <div className="rank-point">1,250 P</div>
+        </div>
+
+      </div>
+
     </div>
-  </div>
   );
 }
 
